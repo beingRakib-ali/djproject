5db73f47e83b54d8c8ec061b02d3ce8976e8d739
Rakib Sarker <201002196@student.green.ac.bd>
2024-09-24 13:16:41 +0600

    GET method

--- Blogs/forms.py
+++ Blogs/forms.py
@@ -1,9 +1,9 @@
 from django import forms
 
 class TeacherFormregistration(forms.Form):
-    frist_name=forms.CharField(label='Enter your frist name')
-    last_name=forms.CharField(label="Enter your last name")
-    email = forms.EmailField(initial="rakibcse201@gmail.com", disabled=True)
+    frist_name=forms.CharField(label='Enter your frist name',label_suffix=' ')
+    last_name=forms.CharField(label="Enter your last name",label_suffix=" ")
+    email = forms.EmailField(initial="rakibcse201@gmail.com",label="Enter your email",label_suffix=' ',disabled=True)
     password=forms.CharField(widget=forms.PasswordInput)
     textarea=forms.CharField(widget=forms.Textarea)
     File=forms.CharField(widget=forms.FileInput)
